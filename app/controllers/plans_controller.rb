72b83948cf2e9f7475e1000752b93c3420354372
liliaim <gouxieai@gmail.com>
2023-08-26 17:57:16 +0900

    読み込み→都道府県ページへの遷移まで

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,7 +1,14 @@
 class PlansController < ApplicationController
   def index
     @plans = Plan.order("created_at DESC") #変更要。start_dateにしたいけどそれだとstart_dateをnull:falseにしなければ
-
   end
-
+  def show
+    @prefecture = Prefecture.find(params[:id])
+    # @spot = Spot.new
+    # @plan = Plan.find(params[:id])
+    @plans = Plan.where(user_id: current_user.id, prefecture_id: @prefecture.id)
+    # @spots = Spot.where(user_id: @plan.user_id, prefecture_id: @plan.prefecture_id)
+   
+  end
+  
 end
