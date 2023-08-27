e1e073ffe78670b4b22b44e432018a9e830aab8a
liliaim <gouxieai@gmail.com>
2023-08-27 22:50:06 +0900

    計画登録機能

--- db/migrate/20230827011434_create_records.rb
+++ db/migrate/20230827011434_create_records.rb
@@ -5,8 +5,8 @@ class CreateRecords < ActiveRecord::Migration[7.0]
       t.string :text                   , null: false
       t.integer :prefecture_id         , null: false
       t.string :city
-      t.date   :start_date             , null: false
-      t.date   :end_date               , null: false
+      t.date   :start_date
+      t.date   :end_date
       t.references :plan               , foreign_key: true
       t.references :user               , null: false, foreign_key: true
       t.timestamps
